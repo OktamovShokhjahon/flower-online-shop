cdfb9324830d62520e4428b86a5c747a2f0fd465
Oktamov Shokhjahon <oktamovshokhjahon@gmail.com>
2025-03-27 19:16:41 +0500

    broker end

---
+++ .next/static/chunks/app_(root)_broker_page_jsx_c07281._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_(root)_broker_page_jsx_c07281._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_(root)_broker_page_jsx_c07281._.js",
+  "chunks": [
+    "static/chunks/app_(root)_062fd9._.js"
+  ],
+  "source": "dynamic"
+});
